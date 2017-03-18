2c4d4204fd0fb4a90a801d3deeaf047e59658208
Sarah Riehl <sls.riehl@gmail.com>
2017-03-18 17:20:03 -0500

    edit gulp

--- gulpfile.js
+++ gulpfile.js
@@ -17,43 +17,29 @@ var banner = ['/*!\n',
 ].join('');
 
 // Compile LESS files from /less into /css
-gulp.task('less', function() {
-    return gulp.src('./less/style.less')
-        .pipe(less())
-        .pipe(header(banner, { pkg: pkg }))
-        .pipe(gulp.dest('./public/css'))
-});
-
-// Minify compiled CSS
-gulp.task('minify-css', ['less'], function() {
-    return gulp.src('./public/css/*.css')
-        .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('./public/css'))
+gulp.task('styles', function() {
+  return gulp.src('./less/style.less')
+    .pipe(less())
+    .pipe(header(banner, { pkg: pkg }))
+    .pipe(gulp.dest('./src/css'))
+    .pipe(cleanCSS({ compatibility: 'ie8'}))
+    .pipe(header(banner, { pkg: pkg }))
+    .pipe(rename({ suffix: '.min'}))
+    .pipe(gulp.dest('./public/css'))
 });
 
 gulp.task('coffee', function() {
   return gulp.src('./coffee/*.coffee')
     .pipe(coffee({bare: true}))
-    .pipe(gulp.dest('./public/js'));
-});
-
-gulp.task('minify-js', function() {
-  return gulp.src('./public/js/index.js')
+    .pipe(header(banner, { pkg: pkg }))
+    .pipe(gulp.dest('./src/js'))
     .pipe(uglify())
     .pipe(header(banner, { pkg: pkg }))
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('./public/js'))
 });
 
-gulp.task('default', ['coffee', 'less', 'minify-css', 
-//'minify-js'
-]);
-
-// Run everything
-// gulp.task('dev', ['less', 'minify-css', 'coffee', 'minify-js'], function() {
-//   gulp.watch('less/*.less', ['less']);
-//   gulp.watch('public/css/*.css', ['minify-css']);
-//   gulp.watch('coffee/*.coffee', ['coffee']);
-//   gulp.watch('public/js/*.js', ['minify-js']);
-// });
+gulp.task('watch', function() {
+  gulp.watch('./coffee/*.coffee', ['coffee']);
+  gulp.watch('./less/style.less', ['styles']);
+});
