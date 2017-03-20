20398a2d1632859b7057ca769b62690e6aff0096
Sarah Riehl <sls.riehl@gmail.com>
2017-03-20 13:16:53 -0500

    refactor user update, comment user controller and helpers

--- controllers/user-helpers.js
+++ controllers/user-helpers.js
@@ -5,47 +5,57 @@ const util = require('util');
 
 const helpers = {
 	updateUser: (req, res, objToUpdate) => {
+		//sync user model
 		models.User.sync()
 		.then(() => {
-			//TODO: bcrypt the password here too
+			//query Users table for email in the session
 			return models.User
 			.findOne({
 				where: { email: req.session.email }
 			})
 			.then((data) => {
+				//if the session email exists in the Users table
 				console.log(data.dataValues);
+				//dehash password
 				const hash = helpers.getHash(req.body.password, data.dataValues.password);
 				console.log(hash);
+				//if current password matches stored hash
 				if(hash) {
 					models.User.sync()
 					.then(() => {
+						//pass object to replace Users table row values, either username,
+						//password, or both, where the email matches the session email
 						return models.User
 						.update(objToUpdate, {
-							//TODO: object destructuring threw an error here, fix maybe?
 							where: { email: req.session.email }
 						})
 						.then((result) => {
-							console.log(`result in update user ${util.inspect(result)}`);
-							//stringify the data object so that we can check the value to determine the message to send
+							//returned object from the update call
+							//console.log(`result in update user ${util.inspect(result)}`);
+							//stringify the data object so that we can check the value
+							//to determine the message to send
+							//can't compare objects/arrays for equality
 							dataStr = JSON.stringify(result);
-							console.log(dataStr)
-							//user mods not saved
+							//console.log(dataStr)
 							if(dataStr === '[0]') {
-								helpers.sessionMessage(req, res, `Info not updated.  Try again.`, 'settings.hbs');
-								//user mods not saved
+								//if no User record was updated
+								helpers.settingsSessMessage(req, res, `Info not updated.  Try again.`, null, null);
 							} else if (dataStr === '[1]') {
-								if(req.body.newEmail) {
-									req.session.email = req.body.newEmail;
-								}
-								helpers.sessionMessage(req, res, `You're golden!  Please use your new credentials to login in the future.`, 'settings.hbs');
+								//if one User record was updated
+								helpers.settingsSessMessage(req, res, `You're golden!  Please use your new credentials to login in the future.`, req.body.newEmail, helpers.setHash(req.body.newPassword));
+							} else {
+								//more than one User record is updated, yikes!
+								helpers.settingsSessMessage(req, res, `Error.`, null, null);
 							}
 						});
 					})
 					.catch((error) => {
-						helpers.sessionMessage(req, res, `Info not updated.  Try again.`, 'settings.hbs');
+						//the update call doesn't return an object
+						helpers.settingsSessMessage(req, res, `Info not updated.  Try again.`, null, null);
 					})
 				} else {
-					helpers.sessionMessage(req, res, `Your current password doesn't match our records.`, 'settings.hbs');
+					//the current password doesn't match the stored hash
+					helpers.settingsSessMessage(req, res, `Your current password doesn't match our records.`, null, null);
 				}
 			})
 
@@ -54,22 +64,41 @@ const helpers = {
 	//bcrypt save password
 	setHash: (password) => {
 		const salt = bcrypt.genSaltSync(10);
-		return bcrypt.hashSync(password, salt);
+		if(password) {
+			return bcrypt.hashSync(password, salt);
+		} else {
+			return false
+		}
 	},
 	//bcrypt retrieve password
 	getHash: (password, hash) => {
 		return bcrypt.compareSync(password, hash);
 	},
+	//create a session object with stored email
 	saveSession: function(req, res, data) {
     req.session.email = data.dataValues.email;
+		req.session.password = data.dataValues.password;
     req.session.cookie.expires = 1000 * 60 * 60 * 24 * 3;
     req.session.save();
   },
+	//save a session message and render to a template,
+	//possibly with the session email
 	sessionMessage: (req, res, message, render) => {
 		req.session.message = message;
 		req.session.save();
-		res.render(render, {data: req.session.message, email: req.session.email});
+		res.render(render, {data: req.session.message});
 	},
+	settingsSessMessage: (req, res, message, email, hash) => {
+		req.session.message = message;
+		if(hash) {
+			req.session.password = hash;
+		}
+		if(email) {
+			req.session.email = email;
+		}
+		req.session.save();
+		req.session.save('settings.hbs', {data: req.session.message, email: req.session.email})
+	}
 }
 
 module.exports = helpers;
--- controllers/user.js
+++ controllers/user.js
@@ -3,30 +3,28 @@ const helpers = require('./user-helpers');
 
 const util = require('util');
 
-//TODO: res.render partials and different template inputs at the same time,
-//from the same res.render
 
 const controller = {
 	//when the root route loads, check the cookie against the session store
 	// and load the signin page or the todo page accordingly
+	// solved this problem with a landing page that sends cookie based on presence
+	// of h2 dom element
 	land: (req, res) => {
-		//how to get the cookie from the front end to compare it to the back end?
 		console.log(`this is req.params.cookie ${req.params.cookie}`);
+		//search the session store for the user's cookie
 		models.ConnectSession.sync()
 		.then(() => {
 			return models.ConnectSession
 			.findOne({
 				where: { sid: req.params.cookie }
 			})
+			//render todo or login page depending on login status from cookie check
 			.then((data) => {
 				console.log(`data.dataValues ${data.dataValues}`);
-				if(data) {
-					res.render('todos.hbs');
-				} else {
-					res.render('login.hbs');
-				}
+				res.render('todos.hbs');
 			});
 		})
+		//in case the cookie check fails, redirect user to login page
 		.catch((error) => {
 			console.log(error);
 			res.render('login.hbs');
@@ -53,18 +51,17 @@ const controller = {
 			return models.User
 			.create(newUser)
 			.then((data) => {
-				//TODO: add cookies and login user on signup. sessions?
-				if(data) {
+				//if signup succeeds, save session, send cookie in header, and render
+				//the todo page
 					req.session.message = 'Signup successful!  Start saving to-dos now.';
 					helpers.saveSession(req, res, data);
 					res.header('Cookie', req.session.id);
 					res.render('todos.hbs', {data: req.session.message});
-				} else {
-					helpers.sessionMessage(req, res, `Signup not successful.`, 'signup.hbs');
-				}
-				console.log(`data from user save ${util.inspect(data)}`);
+				//console.log(`data from user save ${util.inspect(data)}`);
 			});
 		})
+		//if there's some error with the sequelize call,
+		//render the signup page again with a fail message
 		.catch((error) => {
 			helpers.sessionMessage(req, res, `Signup not successful.`, 'signup.hbs');
 		});
@@ -74,7 +71,7 @@ const controller = {
 	loginUser: (req, res) => {
 		//body elements are email and password
 		console.log(req.body);
-		//sync models and authenticate user against the Users table
+		//sequelize call to authenticate user against the Users table
 		models.User.sync()
 		.then(() => {
 			return models.User
@@ -83,37 +80,41 @@ const controller = {
 				where: { email: req.body.email }
 			})
 			.then((data) => {
-				//TODO: send a cookie instead of the data on login. sessions?
-				console.log(`data ${util.inspect(data)}`);
-				if(!data) {
-					helpers.sessionMessage(req, res, `Sorry, your credentials don't match any users.  Please check them and try again.`, 'login.hbs');
+				//console.log(`data ${util.inspect(data)}`);
+				//sequelize call succeeded
+				//compare stored hash to password sent in post request
+				const hash = helpers.getHash(req.body.password, data.dataValues.password);
+				//if the password matches, send cookie in header and
+				//render todo page with the session success message
+				if(hash) {
+					req.session.message = `You have successfully logged in.`;
+					helpers.saveSession(req, res, data);
+					res.header('Cookie', req.session.id);
+					res.render('todos.hbs', {data: req.session.message});
 				} else {
-					//compare stored hash to password sent in post request
-					const hash = helpers.getHash(req.body.password, data.dataValues.password);
-					if(hash) {
-						req.session.message = `You have successfully logged in.`;
-						helpers.saveSession(req, res, data);
-						res.header('Cookie', req.session.id);
-						res.render('todos.hbs', {data: req.session.message});
-					} else {
-						helpers.sessionMessage(req, res, `Sorry, your credentials don't match any users.  Please check them and try again.`, 'login.hbs');
-					}
+					//if there's data but the hash doesn't match the entered password
+					helpers.sessionMessage(req, res, `Sorry, your credentials don't match any users.  Please check them and try again.`, 'login.hbs');
 				}
 			});
 		})
+		//if the sequelize call fails
 		.catch((error) => {
 			console.log(error);
 			helpers.sessionMessage(req, res, `Sorry, your credentials don't match any users.  Please check them and try again.`, 'login.hbs');
 		});
 	},
+	//delete the session object on logout and render the login page
 	logoutUser: (req, res) => {
 		console.log(req.body);
 		req.session.destroy();
 		res.render('login.hbs');
 		//
 	},
+	//display settings page
 	userSettings: (req, res) => {
 		console.log(req.params);
+		//query the session store for the user's session
+		//to display their email address on the settings page
 		models.ConnectSession.sync()
 		.then(() => {
 			return models.ConnectSession
@@ -122,94 +123,112 @@ const controller = {
 			})
 			.then((data) => {
 				console.log(data.dataValues);
+				//the data in the session store is saved as a string,
+				//so parse it into an object
 				sessionObj = JSON.parse(data.dataValues.data);
 				console.log(sessionObj.email);
+				//render email on settings page
 				res.render('settings.hbs', {email: sessionObj.email});
 			});
 		})
+		//if the call to the session store fails
 		.catch((error) => {
 			console.log(error);
 			helpers.sessionMessage(req, res, `Sorry, your credentials don't match any users.  Please check them and try again.`, 'login.hbs');
 		});
 
 	},
+	//change user login info in the Users table
 	updateUser: (req, res) => {
-		//the user will provide either a new password or a new email to update their account
-		console.log(req.body);
+		//the user will provide either a new password or a new email, or both,
+		//to update their account
 		//they must send their current password to authorize them to change the data
+		console.log(req.body);
+		//if they send both
 		if(req.body.newPassword && req.body.newEmail) {
 			const hash = helpers.setHash(req.body.newPassword)
 			const objToUpdate = { password: hash, email: req.body.newEmail };
 			helpers.updateUser(req, res, objToUpdate);
 		} else if(req.body.newPassword) {
-			//pass the new password to the helper function to change it
+			//if they just change the password,
+			//pass the new password to the helper function to change the stored hash
 			const hash = helpers.setHash(req.body.newPassword)
 			const objToUpdate = { password: hash };
 			helpers.updateUser(req, res, objToUpdate);
 		} else if(req.body.newEmail) {
-			//pass the new email to the helper function to change it
+			//if they just change the email
+			//pass the new email to the helper function to change the stored address
 			const objToUpdate = { email: req.body.newEmail };
 			helpers.updateUser(req, res, objToUpdate);
 		} else {
-			helpers.sessionMessage(req, res, 'Please check the data you were trying to change and send it again.', 'settings.hbs');
+			//if neither a new password nor a new email is received
+			helpers.settingsSessMessage(req, res, 'Please check the data you were trying to change and send it again.', null, null);
 		}
 	},
+	//to delete the user and all her todos in the db
 	deleteUser: (req, res) => {
 		console.log(`req.body ${util.inspect(req.body)}`);
+		//sync the users table
 		models.User.sync()
 		.then(() => {
+			//query the session store for the user's email address based on the
+			//cookie stored on the client side
 			return models.ConnectSession
 			.findOne({
 				where: { sid: req.body.cookie }
 			})
 			.then((data) => {
+				//if the cookie is found
 				console.log(`found session? ${util.inspect(data.data)}`);
+				//parse the string in the session data column to an object
 				const emailObj = JSON.parse(data.data);
-				return models.User
-				.findOne({
-					where: { email: emailObj.email }
-				})
-				.then((result) => {
-					console.log(`found user? ${util.inspect(result.dataValues)}`);
-
-					const hash = helpers.getHash(req.body.password, result.dataValues.password);
-					if(hash) {
-						console.log('password is correct');
-						return models.User
-						.destroy({
-							//object destructuring doesn't work here either. pooh!
-							where: { email: emailObj.email }
-						})
-						.then((data) => {
-							//response from deleting the user
-							console.log(`data ${util.inspect(data)}`);
-							console.log(typeof data);
-							if(data === 0) {
-								res.render('login.hbs', {data: `Sorry, your account wasn't deleted.  Please check your credentials and try again.`});
-							} else if(data === 1) {
-								req.session.destroy();
-								res.render('login.hbs', {data: 'Your account and all your to-dos were successfully deleted.'});
-							}
-						})
-						.catch((error) => {
-							console.log('error, destroy user call failed');
-							throw error;
-						});
-					} else {
-						helpers.sessionMessage(req, res, 'Your password is incorrect.  Please try again.', 'login.hbs');
-					}
-				})
-				.catch((error) => {
-					console.log(`error, find user call failed.`);
-					throw error;
-				});
+				//query the Users table for the user stored in the session
+				//if the user is found
+				console.log(`found user? ${util.inspect(emailObj)}`);
+				const hash = helpers.getHash(req.body.password, emailObj.password);
+				//if the password sent to authorize delete matches the stored hash
+				if(hash) {
+					console.log('password is correct');
+					//delete the User and cascade delete all her todos
+					return models.User
+					.destroy({
+						//object destructuring doesn't work here either. pooh!
+						where: { email: emailObj.email }
+					})
+					.then((data) => {
+						//if a response is received from deleting the user
+						//console.log(`data ${util.inspect(data)}`);
+						//console.log(typeof data);
+						if(data === 0) {
+						//the user wasn't deleted
+							helpers.sessionMessage(req, res, `Sorry, your account wasn't deleted.  Please check your credentials and try again.`, 'login.hbs');
+						} else if(data === 1) {
+							//the user was deleted so delete her session as well
+							req.session.destroy();
+							res.render('login.hbs', {data: 'Your account and all your to-dos were successfully deleted.'});
+						}
+					})
+					.catch((error) => {
+						//no data was received from deleting the user
+						console.log('error, destroy user call failed');
+						throw error;
+					});
+				} else {
+					//if the password sent to authorize the user delete
+					//doesn't match the stored hash
+					helpers.sessionMessage(req, res, 'Your password is incorrect.  Please try again.', 'login.hbs');
 			})
 			.catch((error) => {
+				//if the client side cookie is not found in the session store
 				console.log(`error, find session call failed.`);
 				throw error;
 			});
 		})
 		.catch((error) => {
+			//send message to render to the login page if any of the steps fail
+			//render the login page because who knows how far the user got into the
+			//delete process.  logging in again will fail if the user was
+			//actually deleted.
 			helpers.sessionMessage(req, res, 'Error deleting your account.  Please login again.', 'login.hbs');
 			throw error;
 		});
