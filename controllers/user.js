ddf82b23c0ca205c6cb88f7f0399fd7aef89c7df
Sarah Riehl <sls.riehl@gmail.com>
2017-03-28 13:45:13 -0500

    refactor users controller to drop model syncs

--- controllers/user.js
+++ controllers/user.js
@@ -22,20 +22,17 @@ const controller = {
 			password: hash
 		}
 		//sequelize call to save user
-		models.User.sync()
-		.then(() => {
-			return models.User
-			.create(newUser)
-			.then((data) => {
-				//if signup succeeds, save session, send cookie in header, and render
-				//the todo page
-					req.session.message = 'Signup successful!  Start saving to-dos now.';
-					helpers.saveSession(req, res, data);
-					res.header('Cookie', req.session.id);
-					const cookie = req.session.id;
-					contextController.addInitialContexts(req, res, cookie);
-				//console.log(`data from user save ${util.inspect(data)}`);
-			});
+		return models.User
+		.create(newUser)
+		.then((data) => {
+			//if signup succeeds, save session, send cookie in header, and render
+			//the todo page
+			req.session.message = 'Signup successful!  Start saving to-dos now.';
+			helpers.saveSession(req, res, data);
+			res.header('Cookie', req.session.id);
+			const cookie = req.session.id;
+			contextController.addInitialContexts(req, res, cookie);
+			//console.log(`data from user save ${util.inspect(data)}`);
 		})
 		//if there's some error with the sequelize call,
 		//render the signup page again with a fail message
@@ -49,31 +46,28 @@ const controller = {
 		//body elements are email and password
 		console.log(req.body);
 		//sequelize call to authenticate user against the Users table
-		models.User.sync()
-		.then(() => {
-			return models.User
-			.findOne({
-				//obj destructing doesn't work.
-				where: { email: req.body.email }
-			})
-			.then((data) => {
-				console.log(`data ${util.inspect(data)}`);
-				//sequelize call succeeded
-				//compare stored hash to password sent in post request
-				const hash = helpers.getHash(req.body.password, data.dataValues.password);
-				//if the password matches, send cookie in header and
-				//render todo page with the session success message
-				if(hash) {
-					req.session.message = `You have successfully logged in.`;
-					helpers.saveSession(req, res, data);
-					res.header('Cookie', req.session.id);
-					const cookie = req.session.id;
-					readController.readTodos(req, res, cookie);
-				} else {
-					//if there's data but the hash doesn't match the entered password
-					helpers.sessionMessage(req, res, `Sorry, your credentials don't match any users.  Please check them and try again.`, 'login.hbs');
-				}
-			});
+		return models.User
+		.findOne({
+			//obj destructing doesn't work.
+			where: { email: req.body.email }
+		})
+		.then((data) => {
+			console.log(`data ${util.inspect(data)}`);
+			//sequelize call succeeded
+			//compare stored hash to password sent in post request
+			const hash = helpers.getHash(req.body.password, data.dataValues.password);
+			//if the password matches, send cookie in header and
+			//render todo page with the session success message
+			if(hash) {
+				req.session.message = `You have successfully logged in.`;
+				helpers.saveSession(req, res, data);
+				res.header('Cookie', req.session.id);
+				const cookie = req.session.id;
+				readController.readTodos(req, res, cookie);
+			} else {
+				//if there's data but the hash doesn't match the entered password
+				helpers.sessionMessage(req, res, `Sorry, your credentials don't match any users.  Please check them and try again.`, 'login.hbs');
+			}
 		})
 		//if the sequelize call fails
 		.catch((error) => {
@@ -147,62 +141,59 @@ const controller = {
 	deleteUser: (req, res) => {
 		console.log(`req.body ${util.inspect(req.body)}`);
 		//sync the users table
-		models.User.sync()
-		.then(() => {
 			//query the session store for the user's email address based on the
 			//cookie stored on the client side
-			return models.ConnectSession
-			.findOne({
-				where: { sid: req.body.cookie }
-			})
-			.then((data) => {
-				//if the cookie is found
-				console.log(`found session? ${util.inspect(data.data)}`);
-				//parse the string in the session data column to an object
-				const emailObj = JSON.parse(data.data);
-				//query the Users table for the user stored in the session
-				//if the user is found
-				console.log(`found user? ${util.inspect(emailObj)}`);
-				const hash = helpers.getHash(req.body.password, emailObj.password);
-				//if the password sent to authorize delete matches the stored hash
-				if(hash) {
-					console.log('password is correct');
-					//delete the User and cascade delete all her todos
-					return models.User
-					.destroy({
-						//object destructuring doesn't work here either. pooh!
-						where: { email: emailObj.email }
-					})
-					.then((data) => {
-						//if a response is received from deleting the user
-						//console.log(`data ${util.inspect(data)}`);
-						//console.log(typeof data);
-						if(data === 0) {
-						//the user wasn't deleted
-							helpers.sessionMessage(req, res, `Sorry, your account wasn't deleted.  Please check your credentials and try again.`, 'login.hbs');
-						} else if(data === 1) {
-							//the user was deleted so delete her session as well
-							req.session.destroy();
-							res.render('login.hbs', {data: 'Your account and all your to-dos were successfully deleted.', layout: false});
-						}
-					})
-					.catch((error) => {
-						//no data was received from deleting the user
-						console.log('error, destroy user call failed');
-						throw error;
-					});
-				} else {
-					//if the password sent to authorize the user delete
-					//doesn't match the stored hash
-					helpers.sessionMessage(req, res, 'Your password is incorrect.  Please try again.', 'login.hbs');
-				}
-			})
-			.catch((error) => {
-				//if the client side cookie is not found in the session store
-				console.log(`error, find session call failed.`);
-				helpers.sessionMessage(req, res, 'Error deleting your account.  Please login again.', 'login.hbs');
-				throw error;
-			});
+		return models.ConnectSession
+		.findOne({
+			where: { sid: req.body.cookie }
+		})
+		.then((data) => {
+			//if the cookie is found
+			console.log(`found session? ${util.inspect(data.data)}`);
+			//parse the string in the session data column to an object
+			const emailObj = JSON.parse(data.data);
+			//query the Users table for the user stored in the session
+			//if the user is found
+			console.log(`found user? ${util.inspect(emailObj)}`);
+			const hash = helpers.getHash(req.body.password, emailObj.password);
+			//if the password sent to authorize delete matches the stored hash
+			if(hash) {
+				console.log('password is correct');
+				//delete the User and cascade delete all her todos
+				return models.User
+				.destroy({
+					//object destructuring doesn't work here either. pooh!
+					where: { email: emailObj.email }
+				})
+				.then((data) => {
+					//if a response is received from deleting the user
+					//console.log(`data ${util.inspect(data)}`);
+					//console.log(typeof data);
+					if(data === 0) {
+					//the user wasn't deleted
+						helpers.sessionMessage(req, res, `Sorry, your account wasn't deleted.  Please check your credentials and try again.`, 'login.hbs');
+					} else if(data === 1) {
+						//the user was deleted so delete her session as well
+						req.session.destroy();
+						res.render('login.hbs', {data: 'Your account and all your to-dos were successfully deleted.', layout: false});
+					}
+				})
+				.catch((error) => {
+					//no data was received from deleting the user
+					console.log('error, destroy user call failed');
+					throw error;
+				});
+			} else {
+				//if the password sent to authorize the user delete
+				//doesn't match the stored hash
+				helpers.sessionMessage(req, res, 'Your password is incorrect.  Please try again.', 'login.hbs');
+			}
+		})
+		.catch((error) => {
+			//if the client side cookie is not found in the session store
+			console.log(`error, find session call failed.`);
+			helpers.sessionMessage(req, res, 'Error deleting your account.  Please login again.', 'login.hbs');
+			throw error;
 		});
 	}
 }
