97e93ae9514f09b29b81d1fe78efdca82c20e65f
Sarah Riehl <sls.riehl@gmail.com>
2017-04-19 16:13:02 -0500

    update db config

--- models/index.js
+++ models/index.js
@@ -5,15 +5,17 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.JAWSDB_URL || process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
 if(process.env.JAWSDB_URL) {
 	var sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else if(config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+	var config = require(__dirname + '/../config/config.json')[env];
+	if(config.use_env_variable) {
+  	var sequelize = new Sequelize(process.env[config.use_env_variable]);
+	} else {
+  	var sequelize = new Sequelize(config.database, config.username, config.password, config);
+	}
 }
 
 fs
